0df9f71d6ffc5ece2de6ae611ef96c947eecbcfc
Tribayuu <muhamadtribayu@gmail.com>
2025-01-26 22:01:43 +0700

    update

--- conditional statements/if-else.js
+++ conditional statements/if-else.js
@@ -13,3 +13,21 @@ function soal1(angka){
 soal1(9);
 soal1(-12);
 soal1(0);
+
+
+console.log();
+
+// soal 2 apakah sebuah angka genap atau ganjil
+
+function soal2(angka){
+    if(angka % 2 === 0){
+        console.log("bilangan genap");
+   
+    }else {
+        console.log("bilangan ganjil");
+    }
+}
+
+soal2(2);
+soal2(9);
+soal2(5);

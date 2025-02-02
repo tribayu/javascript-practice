87f3557e12450e9d123783c87348c6a5fb99172f
Tribayuu <muhamadtribayu@gmail.com>
2025-02-02 21:53:23 +0700

    add

--- conditional statements/for.js
+++ conditional statements/for.js
@@ -1,3 +1,4 @@
+// Menghitung Faktorial 
 const readline = require('readline');
 
 function hitungFaktorial() {
@@ -24,3 +25,34 @@ function hitungFaktorial() {
 }
 
 hitungFaktorial();
+
+
+console.log()
+
+
+//Menampilkan Tabel Perkalian
+const readline = require('readline');
+
+function tabelPerkalian() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("Masukkan angka untuk tabel perkalian: ", (input) => {
+        let angka = Number(input);
+
+        if (isNaN(angka)) {
+            console.log("Input tidak valid");
+        } else {
+            console.log(`Tabel perkalian untuk ${angka}:`);
+            for (let i = 1; i <= 10; i++) {
+                console.log(`${angka} x ${i} = ${angka * i}`);
+            }
+        }
+
+        rl.close();
+    });
+}
+
+tabelPerkalian();

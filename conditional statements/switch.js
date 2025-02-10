e19ef28b929a27f17e739cd9eb789264be831006
Tribayuu <muhamadtribayu@gmail.com>
2025-02-10 22:01:52 +0700

    add

--- conditional statements/switch.js
+++ conditional statements/switch.js
@@ -22,5 +22,31 @@ const readline = require("readline")
                     default:
                     console.log("hari tidak di kenal");
          }
-         rl.close();
-    });
+         
+
+    console.log()
+
+//////////////
+
+rl.question("Masukkan angka (1-12) untuk bulan: ", function (input) {
+    let bulan = parseInt(input);
+
+    switch (bulan) {
+        case 1: console.log("Januari"); break;
+        case 2: console.log("Februari"); break;
+        case 3: console.log("Maret"); break;
+        case 4: console.log("April"); break;
+        case 5: console.log("Mei"); break;
+        case 6: console.log("Juni"); break;
+        case 7: console.log("Juli"); break;
+        case 8: console.log("Agustus"); break;
+        case 9: console.log("September"); break;
+        case 10: console.log("Oktober"); break;
+        case 11: console.log("November"); break;
+        case 12: console.log("Desember"); break;
+        default: console.log("Bulan tidak valid");
+    }
+
+    rl.close();
+   });
+});

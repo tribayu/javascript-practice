61d81903f9bf5183775bf22f6ebaf8ad90d58739
Tribayuu <muhamadtribayu@gmail.com>
2025-02-13 22:21:13 +0700

    add file

---
+++ basic/data-types.js
@@ -0,0 +1,8 @@
+let stringType ="hello, bay";
+let numberType = 21;
+let booleanType = true;
+let arrayType = [1, 2, 3, 4, 5];
+let objectType = {name: "bay", age: 21 };
+
+console.log(typeof stringType, typeof numberType, typeof booleanType, typeof objectType);
+console.log(Array.isArray(arrayType));

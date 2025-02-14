84e4b8acec28ad43c95a2e5529389d97811d106b
Tribayuu <muhamadtribayu@gmail.com>
2025-02-14 20:05:40 +0700

    add

---
+++ basic/operators.js
@@ -0,0 +1,12 @@
+//  operator aritmatika
+let a = 10;
+let b = 7;
+console.log(a + b, a - b, a * b, a / b, a % b);
+
+//  operator perbandingan 
+console.log( a > b, a < b, a == b, a === b, a != b );
+
+// operator logika
+let x = true;
+let y = false;
+console.log(x && y, x || y, !x);

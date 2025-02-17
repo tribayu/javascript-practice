52c1f8d2c574525cbb846b7d21d2e4c8a42e2750
Tribayuu <muhamadtribayu@gmail.com>
2025-02-17 20:00:57 +0700

    add

--- basic/data-types.js
+++ basic/data-types.js
@@ -6,3 +6,10 @@ let objectType = {name: "bay", age: 21 };
 
 console.log(typeof stringType, typeof numberType, typeof booleanType, typeof objectType);
 console.log(Array.isArray(arrayType));
+
+console.log();
+///1
+let easy1 = "123";
+let easy2 = 123;
+console.log(typeof easy1);
+console.log(typeof easy2);

05f413eddae883d97254c2f7932f1695158dd395
Tribayuu <muhamadtribayu@gmail.com>
2025-02-21 22:06:52 +0700

    add-function

--- basic/functions.js
+++ basic/functions.js
@@ -7,14 +7,26 @@ function defaultFunction (){
     console.log("ini adalah fungsi tanpa parameter")
 }
 
+console.log();
+
 // Fungsi dengan parameter
 function greet(name) {
     return `hello, ${name}!`;
 }
 console.log(greet("bay"));
 
+console.log();
+
 // Fungsi dengan nilai default parameter
 function add(a = 2, b = 3) {
     return a + b;
 }
-console.log(ad(), add(7, 9));
+console.log(add(), add(7, 9));
+
+console.log();
+
+//fungsi yang mengembalikan kuadrat dari sebuah angka.
+function square(num) {
+    return num * num;
+}
+console.log(square(15));

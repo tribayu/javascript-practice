a8496a09edac7dcb9cd54e076af6a533de918aa1
Tribayuu <muhamadtribayu@gmail.com>
2025-04-08 22:44:41 +0700

    add-functions

--- erroe-handling/custom-errors.js
+++ erroe-handling/custom-errors.js
@@ -10,3 +10,27 @@ try {
 } catch (error) {
     console.error(`${error.name}: ${error.message}`);
 }
+
+
+
+//////////////////
+
+
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError";
+    }
+}
+
+function checkAge(age) {
+    if (age < 18) throw new ValidationError("Age must be at least 18");
+    return true;
+}
+
+try {
+    checkAge(15);
+} catch (err) {
+    console.error(`${err.name}: ${err.message}`);
+}
+

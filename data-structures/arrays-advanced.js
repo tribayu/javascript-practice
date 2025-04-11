311a1716b61d79315c40af20c960f615b43461f9
Tribayuu <muhamadtribayu@gmail.com>
2025-04-11 21:20:06 +0700

    add-functions

--- data-structures/arrays-advanced.js
+++ data-structures/arrays-advanced.js
@@ -1,3 +1,14 @@
 const numbers = [1, 2, 3, 4, 5];
 const squared = numbers.map(num => num * num);
 console.log("Squared:", squared);  // Output: [1, 4, 9, 16, 25]
+
+///////////////////////
+
+
+const nums = [1, 2, 3, 4, 5];
+
+const sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log("Sum:", sum);
+
+const allPositive = nums.every(num => num > 0);
+console.log("All Positive:", allPositive);
--- data-structures/maps-and-sets.js
+++ data-structures/maps-and-sets.js
@@ -5,3 +5,18 @@ const map = new Map();
 map.set("name", "Alice");
 map.set("age", 30);
 console.log("Map:", map);
+
+
+
+/////////////////
+
+
+
+const mySet = new Set([1, 2, 3, 3]);
+mySet.add(4);
+console.log(mySet);
+
+const myMap = new Map();
+myMap.set("a", 1);
+myMap.set("b", 2);
+console.log(myMap.get("a"));

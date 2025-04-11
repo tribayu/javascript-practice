d07a84b8dc5cd7b5b912ef270d53a74d54909926
Tribayuu <muhamadtribayu@gmail.com>
2025-04-11 21:20:46 +0700

    add-functions

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -37,3 +37,48 @@ list.add(1);
 list.add(2);
 list.add(3);
 list.print();
+
+
+
+/////////////////////
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
+
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.print();

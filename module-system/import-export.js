47118d76f5471ab503a8ea7f8645cde9bd757c7a
Tribayuu <muhamadtribayu@gmail.com>
2025-04-13 23:17:57 +0700

    add-functions

--- module-system/import-export.js
+++ module-system/import-export.js
@@ -5,3 +5,11 @@ export function add(a, b) {
 export function subtract(a, b) {
     return a - b;
 }
+
+
+
+
+///////////////////
+
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;

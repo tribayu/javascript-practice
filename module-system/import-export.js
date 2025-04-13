9e0fdfb2e7ba8810abae6d95770d056891542ee0
Tribayuu <muhamadtribayu@gmail.com>
2025-04-13 23:18:40 +0700

    add-functions

--- module-system/import-export.js
+++ module-system/import-export.js
@@ -13,3 +13,9 @@ export function subtract(a, b) {
 
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
+
+
+import { add, subtract } from './math.js';
+
+console.log(add(10, 5));       // 15
+console.log(subtract(10, 5));  // 5
--- module-system/module-pattern.js
+++ module-system/module-pattern.js
@@ -11,3 +11,20 @@ const CounterModule = (() => {
 CounterModule.increment();
 CounterModule.increment();
 console.log("Count:", CounterModule.getCount());
+
+
+////////////////////
+
+const Counter = (() => {
+    let count = 0;
+
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        getCount: () => count
+    };
+})();
+
+Counter.increment();
+Counter.increment();
+console.log(Counter.getCount()); // 2
